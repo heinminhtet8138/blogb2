6fb921f7862c6b62192307850b710ec05f652d9d
heinminhtet8138 <heinminhtet8138@gmail.com>
2023-07-04 20:26:00 +0630

    create post

---
+++ backend/create_post.php
@@ -0,0 +1,101 @@
+<?php 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+    include "../dbconnect.php";
+
+    $sql = "SELECT * FROM categories";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $categories = $stmt->fetchAll();
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    $title = $_POST['title'];
+    $category_id = $_POST['category_id'];
+    $user_id = 2;
+    $description = $_POST['description'];
+    $photo_arr = $_FILES['photo'];
+
+    // echo "$title and $category_id and $user_id and $description";
+    // print_r($photo_arr);
+
+    if(isset($photo_arr) && $photo_arr['size'] > 0){
+        $dir = 'images/';
+        $photo = $dir.$photo_arr['name']; // images/photo_name.png
+        
+        $tmp_name = $photo_arr['tmp_name'];
+        move_uploaded_file($tmp_name,$photo);
+    };
+
+
+    $sql = "INSERT INTO posts (title,category_id,user_id,photo,description) VALUES(:title, :category, :user, :photo, :description)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':title',$title);
+    $stmt->bindParam(':category',$category_id);
+    $stmt->bindParam(':user',$user_id);
+    $stmt->bindParam(':photo',$photo);
+    $stmt->bindParam(':description',$description);
+    $stmt->execute();
+
+    header("location: create_post.php");
+    exit;
+
+}else {
+
+    include "layouts/nav_sidebar.php";
+
+?>
+
+    <main>
+        <div class=" container-fluid px-3">
+            <div class="card my-5">
+                <div class="card-header">
+                    <p class="d-inline">Post Create</p>
+                    <a href="posts.php" class="btn btn-sm btn-danger float-end">Cancel</a>
+                </div>
+                <div class="card-body">
+                    <form action="<?php htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post" enctype="multipart/form-data">
+                        <div class="mb-3">
+                            <label for="title" class="form-label">Title</label>
+                            <input type="text" class="form-control" id="title" name="title">
+                        </div>
+                        <div class="mb-3">
+                            <label for="photo" class="form-label">Photo</label>
+                            <input type="file" class="form-control" id="photo" name="photo">
+                        </div>
+                        <div class="mb-3">
+                            <label for="category_id">Category</label>
+                            <select class="form-select" name="category_id" id="category_id">
+                                <option selected>Select Category</option>
+                                <?php 
+                                    foreach ($categories as $category) { 
+                                ?>
+                                    <option value="<?= $category['id'] ?>"><?= $category['name'] ?></option>
+
+                                <?php 
+                                    }
+                                ?>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="description" class="form-label">Description</label>
+                            <textarea name="description" id="description" class="form-control"></textarea>
+                        </div>
+                        <div class="d-grid gap-2">
+                            <button class="btn btn-primary" type="submit">Submit</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </main>
+
+<?php 
+
+    include "layouts/footer.php";
+
+}
+
+?>
--- backend/posts.php
+++ backend/posts.php
@@ -1,11 +1,76 @@
 <?php 
-
     include "layouts/nav_sidebar.php";
+    include "../dbconnect.php";
 
+    $sql = "SELECT posts.*, categories.name as c_name, users.name as u_name FROM posts INNER JOIN categories ON posts.category_id = categories.id INNER JOIN users ON posts.user_id = users.id";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $posts = $stmt->fetchAll();
+
+    // var_dump($posts);
 ?>
+                <main>
+                    <div class="container-fluid px-4">
+                        <div class="mt-3">
+                            <h1 class="mt-4 d-inline">Posts</h1>
+                            <a class="btn btn-primary float-end" href="create_post.php">Create Post</a>
+                        </div>
+                        
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item"><a href="index.php">Dashboard</a></li>
+                            <li class="breadcrumb-item active">Posts</li>
+                        </ol>
+                        <div class="card mb-4">
+                            <div class="card-header">
+                                <i class="fas fa-table me-1"></i>
+                               Posts List
+                            </div>
+                            <div class="card-body">
+                                <table id="datatablesSimple">
+                                    <thead>
+                                        <tr>
+                                            <th>Title</th>
+                                            <th>Category</th>
+                                            <th>Created By</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tfoot>
+                                        <tr>
+                                        <th>Title</th>
+                                            <th>Category</th>
+                                            <th>Created By</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </tfoot>
+                                    <tbody>
+                                        <?php 
+                                            foreach ($posts as $post) {
+                                        ?>
 
-    
+                                                <tr>
+                                                    <td><?= $post['title'] ?></td>
+                                                    <td><?= $post['c_name'] ?></td>
+                                                    <td><?= $post['u_name'] ?></td>
+                                                    <td>
+                                                        <button class="btn btn-sm btn-warning">Edit</button>
+                                                        <button class="btn btn-sm btn-danger">Delete</button>
+                                                    </td>
+
+                                                </tr>
+
+                                        <?php 
+                                            }
+                                        ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </main>
 
 <?php 
+
     include "layouts/footer.php";
+
 ?>
